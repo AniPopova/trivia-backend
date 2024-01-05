89aa4ded44eebe014961174cfd3b8184af3ac0f7
Ana Popova <ana.popova83@gmail.com>
2024-01-05 14:41:57 +0200

    "feat(design): arranging project structure"

---
+++ migrations/20240105095311-create-question.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Questions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      categoryId: {
+        type: Sequelize.UUID
+      },
+      difficultyId: {
+        type: Sequelize.UUID
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Questions');
+  }
+};
